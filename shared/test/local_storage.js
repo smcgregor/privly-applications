a46e0975e6df7d7cf454d5a8f9960d8e340d3a5a
Manraj Singh <manrajsinghgrover@gmail.com>
2015-04-06 23:04:10 +0530

    Fixes indentation.

--- shared/test/local_storage.js
+++ shared/test/local_storage.js
@@ -4,31 +4,31 @@
  * This spec is managed by the Jasmine testing library.
  **/
 describe ("Local Storage Test Suite", function() {
-  
- it("can assign to localStorage and get the item back", function() {
-   ls.setItem("hello", "world");
-   expect(ls.getItem("hello")).toBe("world");
-   ls.removeItem("hello");
- });
 
- it("knows localStorage is not defined in Xul pages", function() {
-   if( privlyNetworkService.platformName() === "FIREFOX" ) {
-     expect(ls.localStorageDefined)
+  it("can assign to localStorage and get the item back", function() {
+    ls.setItem("hello", "world");
+    expect(ls.getItem("hello")).toBe("world");
+    ls.removeItem("hello");
+  });
+
+  it("knows localStorage is not defined in Xul pages", function() {
+    if( privlyNetworkService.platformName() === "FIREFOX" ) {
+      expect(ls.localStorageDefined)
           .toBe(false);
-   } else {
-     expect(ls.localStorageDefined)
+    } else {
+      expect(ls.localStorageDefined)
           .toBe(true);
-   }
- });
+    }
+  });
 
- it("can remove items from storage", function() {
+  it("can remove items from storage", function() {
     ls.setItem("hello", "world");
     expect(ls.getItem("hello")).toBe("world");
     ls.removeItem("hello");
     expect(ls.getItem("hello")).not.toBeDefined();
- });
+  });
 
- it("can set and get Objects", function() {
+  it("can set and get Objects", function() {
     var obj = {"foo": "bar", "bar": "bazz"};
     ls.setItem("key", obj);
     var returned = ls.getItem("key");
@@ -36,9 +36,9 @@ describe ("Local Storage Test Suite", function() {
     expect(returned["foo"]).toBe("bar");
     expect(returned["bar"]).toBe("bazz");
     expect(returned).toEqual(obj);
- });
+  });
 
- it("can set and remove Objects", function() {
+  it("can set and remove Objects", function() {
     var obj = {"foo": "bar", "bar": "bazz"};
     ls.setItem("key", obj);
     var returned = ls.getItem("key");
@@ -47,6 +47,6 @@ describe ("Local Storage Test Suite", function() {
     expect(returned["bar"]).toBe("bazz");
     ls.removeItem("key");
     expect(ls.getItem("key")).not.toBeDefined();
- });
- 
+  });
+
 });
