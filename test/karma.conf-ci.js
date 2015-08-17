00be42bb3436eda8e10b1124b50e191f64b1da0f
Sean McGregor <smcgregor08@cmc.edu>
2015-08-17 13:12:02 -0700

    specify platform for Firefox Karma tests

--- test/karma.conf-ci.js
+++ test/karma.conf-ci.js
@@ -77,6 +77,7 @@ module.exports = function(config) {
   var sl_firefox = {
     base: 'SauceLabs',
     browserName: 'firefox',
+    platform: 'Windows 7',
     version: 'beta'
   }
   var sl_safari = {
